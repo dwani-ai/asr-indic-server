4e7567b5edc7fb5dfa05a20a65765111c47237cc
sachin <26170834+sachinsshetty@users.noreply.github.com>
2026-02-22 18:38:20 +0100

    test-multiple-langues

--- asr-code.py
+++ asr-code.py
@@ -9,6 +9,13 @@ model = AutoModel.from_pretrained(
     trust_remote_code=True
 )
 
+
+
+"""
+Process Language - Kannada
+"""
+
+print("Process Language - Kannada----------")
 # Load & preprocess audio (unchanged)
 wav, sr = torchaudio.load("samples/kannada_sample_1.wav")
 wav = torch.mean(wav, dim=0, keepdim=True)
@@ -20,3 +27,89 @@ if sr != 16000:
 # ASR with both decoders
 print("CTC:", model(wav, "kn", "ctc"))
 print("RNNT:", model(wav, "kn", "rnnt"))
+
+print("-----")
+"""
+Process Language - Hindi
+"""
+
+print("Process Language - Hindi")
+# Load & preprocess audio (unchanged)
+wav, sr = torchaudio.load("samples/hindi_sample.wav")
+wav = torch.mean(wav, dim=0, keepdim=True)
+
+if sr != 16000:
+    resampler = torchaudio.transforms.Resample(sr, 16000)
+    wav = resampler(wav)
+
+# ASR with both decoders
+print("CTC:", model(wav, "hi", "ctc"))
+print("RNNT:", model(wav, "hi", "rnnt"))
+
+print("-----")
+
+
+"""
+Process Language - Marathi
+"""
+print("Process Language - Marathi----")
+# Load & preprocess audio (unchanged)
+wav, sr = torchaudio.load("samples/marathi_sample.wav")
+wav = torch.mean(wav, dim=0, keepdim=True)
+
+if sr != 16000:
+    resampler = torchaudio.transforms.Resample(sr, 16000)
+    wav = resampler(wav)
+
+# ASR with both decoders
+print("CTC:", model(wav, "mr", "ctc"))
+print("RNNT:", model(wav, "mr", "rnnt"))
+
+
+print("-----")
+
+
+
+"""
+Process Language - Tamil
+"""
+
+print("Process Language - Tamil ----")
+# Load & preprocess audio (unchanged)
+wav, sr = torchaudio.load("samples/tamil_sample.wav")
+wav = torch.mean(wav, dim=0, keepdim=True)
+
+if sr != 16000:
+    resampler = torchaudio.transforms.Resample(sr, 16000)
+    wav = resampler(wav)
+
+# ASR with both decoders
+print("CTC:", model(wav, "ta", "ctc"))
+print("RNNT:", model(wav, "ta", "rnnt"))
+
+
+print("-----")
+
+
+
+
+"""
+Process Language - Telugu
+"""
+
+print("Process Language - Telugu ---")
+# Load & preprocess audio (unchanged)
+wav, sr = torchaudio.load("samples/telugu_sample.wav")
+wav = torch.mean(wav, dim=0, keepdim=True)
+
+if sr != 16000:
+    resampler = torchaudio.transforms.Resample(sr, 16000)
+    wav = resampler(wav)
+
+# ASR with both decoders
+print("CTC:", model(wav, "te", "ctc"))
+print("RNNT:", model(wav, "te", "rnnt"))
+
+
+print("-----")
+
